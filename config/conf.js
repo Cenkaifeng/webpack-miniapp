2be84633a5480119a2fed43ff68168c3fecc60b9
jerviscen <jerviscen@tenvine.com>
2019-07-17 11:18:05 +0800

    [ADD] 增加新建页面配置命令

---
+++ config/conf.js
@@ -0,0 +1,17 @@
+// 环境
+exports.process = {
+  envs: ['dev', 'production'],
+  host: {
+    'dev': 'test.com.cn'
+  }
+}
+
+// 路径
+exports.path = {
+  tmp: '.tmp',
+  src: './src',
+  dist: './dist',
+  serve: './serve',
+  gulp: './gulp'
+};
+
