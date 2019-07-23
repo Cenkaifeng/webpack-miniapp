1dd3a58fb76123eff62a6a6727bf74bd3196b196
cenkaifeng <cenkaifeng@jingdaka.com>
2019-07-23 11:18:32 +0800

    [UPDATE] 更新readme

--- build/plugin/HellowPlugin.js
+++ build/plugin/HellowPlugin.js
@@ -3,8 +3,8 @@ class HelloWorldPlugin {
     compiler.hooks.done.tap('Hello World Plugin', (
       stats /* 在 hook 被触及时，会将 stats 作为参数传入。 */
     ) => {
-      console.log('Hello World!');
-      console.log('环境'+ process.env.NODE_ENV, '构建类型'+process.env.BUILD_TYPE);
+    //   console.log('环境'+ process.env.NODE_ENV, '构建类型'+process.env.BUILD_TYPE);
+      console.log('环境'+ process.env.NODE_ENV);
     });
   }
 }
